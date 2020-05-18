771469916e8f8a7e07c2a358d3875a5d7569089b
gytanzo <gytanzo@gmail.com>
2020-05-18 00:40:18 -0400

    Good warmup.

--- getpost.py
+++ getpost.py
@@ -4,23 +4,29 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-weekday = datetime.today().strftime('%A')
+
+def my_round(x, base = 5):
+    return base * round(x/base)
 
 
-def warmup():
-    if weekday == "Sunday":
-        return "Silly goose, it's a Sunday. You don't have a warmup."
-    elif weekday == "Monday":
-        message = "Today's T1 is Bench Press. This is your warmup." + "\n" + "\n"
-        file = open('Message.txt', 'r')
-        lines = file.readlines()
-        for line in lines:
-            message += line
-        return message
+def warmup(value):
+    message = "Here is your warmup." + "\n" + "\n"
+    message += str(my_round(value * 0.4)) + "x5"
+    message += str(my_round(value * 0.5)) + "x5"
+    message += str(my_round(value * 0.6)) + "x5"
+    return message
 
 
 @app.route('/', methods=['GET', 'POST'])
 def incoming_sms():
+    weekday = datetime.today().strftime('%A')
+    file = open('Current Values.txt', 'r')
+    lines = file.readlines()
+    squat = int(lines[0].rstrip())
+    bench = int(lines[1].rstrip())
+    deadlift = int(lines[2].rstrip())
+    press = int(lines[3].rstrip())
+
     """Send a dynamic reply to an incoming text message"""
     # Get the message the user sent our Twilio number
     body = request.values.get('Body', None)
@@ -30,16 +36,18 @@ def incoming_sms():
 
     # Determine the right reply for this message
     if body == 'Warmup' or body == 'warmup' or body == 'Warmup ' or body == 'warmup ':
-        resp.message(warmup())
-    elif body == 'bye':
-        resp.message("Goodbye")
+        if weekday == "Sunday":
+            resp.message("Silly goose, it's a Sunday. You don't have a warmup.")
+        elif weekday == "Monday":
+            resp.message(warmup(bench))
+    elif body == 'Workout' or body == 'workout' or body == 'Workout ' or body == 'workout ':
+        resp.message("goodbye")
     else:
         string = ""
         for character in body:
             if character.isdigit():
                 string += character
         resp.message(string)
-
     return str(resp)
 
 
