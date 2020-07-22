a673bfafb4e922cabd65701b1f4076eeab7433a3
gytanzo <gytanzo@gmail.com>
2020-07-22 17:38:59 -0400

    Maybe consolidating it will help?

--- getpost.py
+++ getpost.py
@@ -222,8 +222,7 @@ def incoming_sms():
         if user == "":
             if re.search('initial', body, re.IGNORECASE) is not None:
                 if re.search('name', body, re.IGNORECASE) is not None:  # They received the welcome message.
-                    name = re.sub("initial", '', body, re.IGNORECASE)  # Remove the "initial" part of the string.
-                    name = re.sub("name", '', name, re.IGNORECASE)  # Remove the "name" part of the string.
+                    name = re.sub("initial name", '', body, re.IGNORECASE)  # Remove the "initial" part of the string.
                     name = "".join(name.split())  # Remove all whitespaces from string.
 
                     name_file = open("Names.txt", 'a')
