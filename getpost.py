0476024899abb7a322cd3fdf812e3346e9a3c9f9
gytanzo <gytanzo@gmail.com>
2020-07-22 15:35:04 -0400

    Checking code bit by bit.

--- getpost.py
+++ getpost.py
@@ -207,12 +207,6 @@ def incoming_sms():
     names = name_file.readlines()
     name_file.close()
 
-    for line in names:
-        if re.search(phone_number, line, re.IGNORECASE) is not None:
-            line_copy = line
-            line_copy = re.sub(phone_number, '', line_copy, re.IGNORECASE)  # Remove phone number from string.
-            line_copy = re.sub(", ", '', line_copy, re.IGNORECASE)  # Remove trailing characters, should just be name now
-            user = line_copy
 
     resp = MessagingResponse()
 
